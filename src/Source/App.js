4f0ae266a06929d0357eef10c1d22c140bd0a609
unknown <200030059@iitdh.ac.in>
2022-10-15 03:34:38 +0530

    Added documentaion page

---
+++ src/Source/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Firebase</Text>
+      <Text>https://firebase.google.com/docs</Text>
+    </View>
+  );
+};
+
+export default App;
