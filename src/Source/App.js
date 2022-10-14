c9e20aa097797bc45d3f83704f431f35799752a3
unknown <200030059@iitdh.ac.in>
2022-10-15 03:39:37 +0530

    Added documentaion page for firbase

--- src/Source/App.js
+++ src/Source/App.js
@@ -6,6 +6,19 @@ const App = () => {
     <View>
       <Text>Firebase</Text>
       <Text>https://firebase.google.com/docs</Text>
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Firbase link </title>
+        <link rel="stylesheet" href="https://firebase.google.com/docs" />
+      </head>
+      <body>
+        
+      </body>
+      </html>
     </View>
   );
 };
